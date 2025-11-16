abd8ca1a246590a3fbbfff084eab8138c7d3fa7b
Adarsh verma <av969998@gmail.com>
2025-11-16 11:54:26 +0530

    Add files via upload

--- Hero.js
+++ Hero.js
@@ -1,22 +1,22 @@
-// import React from 'react';
+import React from 'react';
 
-// function Hero() {
-//     return ( 
-//         <div className='container p-5'>
-//              <div className='row text-center'>
-//                 <img src='src/media/images/homeHero.png' alt='Hero Image' className='mb-5' />
-//                  <h1 className='mt-5'>Invest in everything</h1>
+function Hero() {
+    return ( 
+        <div className='container p-5'>
+             <div className='row text-center'>
+                <img src="media/homeHero.png" alt='Hero Image' className='mb-5' />
+                 <h1 className='mt-5'>Invest in everything</h1>
 
-//                  <p>Online platform to invest in stocks, derivatives, mutual funds, and
-//                      more</p>
+                 <p>Online platform to invest in stocks, derivatives, mutual funds, and
+                     more</p>
                     
-//                  <button className='p-2 btn btn-primary fs-5' style={{ width: "20%", margin: "0 auto" }}>Signup now</button>
+                 <button className='p-2 btn btn-primary fs-5' style={{ width: "20%", margin: "0 auto" }}>Signup now</button>
 
-//              </div>
+             </div>
 
-//          </div>
-//      );
-// }
+         </div>
+     );
+}
 
-// export default Hero;
+export default Hero;
 
